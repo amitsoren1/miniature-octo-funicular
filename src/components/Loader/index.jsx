360515f26be13c22cc27ebe5039d1321d33d280c
amit <amit@setupenzyme.com>
2021-12-23 01:24:19 +0530

    added axios

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/main.css";
 import Icon from "../Icon";
+import Axios from "axios";
 
 const Loader = ({ done }) => {
+	const tokenStr = ""
+	const [progress, setProgress] = useState(0)
+
+	useEffect(()=>{
+		async function fetch(){
+		  let result = await Axios.get('/cha', {
+			headers: {"Authorization" : `Token ${tokenStr}`},
+			onDownloadProgress: (progressEvent) => {
+			  let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+			//   console.log(progressEvent.lengthComputable)
+			setProgress(percentCompleted)
+			  console.log(percentCompleted);
+			}
+		  })
+		  .then(res => {
+			console.log("All DONE: ", res.headers)
+			// console.log(res.data)
+		  })
+		}
+	fetch()
+	}, [])
+
 	return (
 		<div className="loader">
 			<div className="loader__logo-wrapper">
 				<Icon id="whatsapp" className="loader__logo" />
 			</div>
-			<div
-				className={`loader__progress ${done ? "loader__progress--done" : ""}`}
-			></div>
+			<div className="loader__container">
+				<div className="loader__progress" style={{width: `${progress}%`}}></div>
+			</div>
 			<h1 className="loader__title"> Whatsapp</h1>
 			<p className="loader__desc">
 				<Icon id="lock" className="loader__icon" />
